0849e1c6c664e08c370802635e19b3d8207d9e96
Wansmer <speakmed@gmail.com>
2020-03-13 22:03:36 +0300

    git mistake

--- server/api/conf/conf.js
+++ server/api/conf/conf.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 const BASE_URL = 'https://hw.shri.yandex/api/';
-const MYAPIKEY = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjQ2OTc3MTczIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6IldhbnNtZXIiLCJ1cm46Z2l0aHViOnVybCI6Imh0dHBzOi8vYXBpLmdpdGh1Yi5jb20vdXNlcnMvV2Fuc21lciIsIm5iZiI6MTU4NDA5NTYzNCwiZXhwIjoxNTg2Njg3NjM0LCJpc3MiOiJTaHJpLUhvbWV3b3JrIiwiYXVkIjoiU2hyaS1Ib21ld29yayJ9.1YC9weT9bdj2Alx4ehYVIOPOq67cIj2Sd28g3jqNz2k';
 const headers = {
   accept: 'application/json',
   Authorization: MYAPIKEY
