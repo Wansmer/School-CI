c87302d4902b34e5b6017197a04e4e799b7af941
Wansmer <wansmer@gmail.com>
2020-03-25 22:47:03 +0300

    start input

--- client/src/blocks/Form/Form.js
+++ client/src/blocks/Form/Form.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import './Form.scss';
+import Input from '../Input/Input';
+
+const inputReqClasses = {
+  elems: {
+    Label: {
+      mods: {
+        required: ''
+      }
+    },
+    Input: {
+      mods: {
+        border: 'default'
+      }
+    }
+  }
+}
 
 function Form(props) {
   return (
@@ -9,26 +25,34 @@ function Form(props) {
         <p className="Form-Text">Configure repository connection and&#160;synchronization settings.</p>
       </div>
       <div className="Form-Field">
-
+        <Input 
+          type='text'
+          name='repo'
+          id='repo_id'
+          placeholder='user-name/repo-name'
+          labelText='GitHub repository'
+          labelFor='repo_id'
+          classes={inputReqClasses}
+        />
       </div>
-        <div className="Form-Field">
-
-        </div>
-        <div className="Form-Field">
-
-        </div>
-        <div className="Form-Field">
-
-        </div>
-        <div className="Form-Field Content-Form-Field">
-          <a href="/build-history.html" className="Button Button_size_l Button_type_action Form-Button">
-            <span className="Button-Text">Save</span>
-          </a>
-          <a href="/#" className="Button Button_size_l Button_type_control Form-Button">
-            <span className="Button-Text">Cancel</span>
-          </a>
-        </div>
-      </form>
+      <div className="Form-Field">
+        <Input />
+      </div>
+      <div className="Form-Field">
+        <Input />
+      </div>
+      <div className="Form-Field">
+        <Input />
+      </div>
+      <div className="Form-Field Content-Form-Field">
+        <a href="/build-history.html" className="Button Button_size_l Button_type_action Form-Button">
+          <span className="Button-Text">Save</span>
+        </a>
+        <a href="/#" className="Button Button_size_l Button_type_control Form-Button">
+          <span className="Button-Text">Cancel</span>
+        </a>
+      </div>
+    </form>
   )
 }
 
--- client/src/blocks/Input/Input.js
+++ client/src/blocks/Input/Input.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import './Input.scss';
 
+import { expandClasses } from '../../utils';
+
+
 function Input(props) {
   return (
-    <div className="Input Input_direction_column">
-      <label htmlFor="repo_id" className="Input-Label Input-Label_required">GitHub repository</label>
+    <div className={expandClasses(props.classes, 'Input')}>
+      <label htmlFor={props.labelFor}
+        className={'Input-Label ' +
+          expandClasses(props.classes, 'Input', 'Label')
+        }>
+        {props.labelText}
+      </label>
       <input type="text" name="repo" id="repo_id" className="Input-Input Input-Input_border_default" placeholder="user-name/repo-name" required />
       {/* <span className="Input-Icon Icon Icon_inputClear"></span> */}
     </div>
   )
+}
+
+Input.defaultProps = {
+  classes: {
+    mods: {
+      direction: 'column'
+    }
+  }
 }
 
 export default Input;
