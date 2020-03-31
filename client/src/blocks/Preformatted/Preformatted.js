83ea0d9b2b23a24236c93f189621a742e5d78dba
Wansmer <wansmer@gmail.com>
2020-04-01 02:58:16 +0300

    change pre for white-space: pre

--- client/src/blocks/Preformatted/Preformatted.js
+++ client/src/blocks/Preformatted/Preformatted.js
@@ -6,11 +6,9 @@ function Preformatted(props) {
   const convert = new Convert();
   const res = convert.toHtml(props.children);
   return (
-    <pre className="Preformatted TicketList-Log">
-      <code>
-        {res || 'Waiting...'}
-      </code>
-    </pre>
+    <div className="Preformatted TicketList-Log">
+      {res || 'Waiting...'}
+    </div>
   )
 }
 
