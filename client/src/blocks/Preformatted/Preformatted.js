69abf1a11276ceb61bd1d1c39568deb97a3f15d2
Wansmer <wansmer@gmail.com>
2020-04-01 22:20:22 +0300

    fix view of stdout in pre

--- client/src/blocks/Preformatted/Preformatted.js
+++ client/src/blocks/Preformatted/Preformatted.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Convert from 'ansi-to-html';
 import './Preformatted.scss';
 
+const settingsAnsi = {
+  fg: '#000',
+  bg: '#FFF'
+};
+
 function Preformatted(props) {
-  const convert = new Convert();
+  const convert = new Convert(settingsAnsi);
   const res = convert.toHtml(props.children);
+  const html = { __html: res };
   return (
-    <div className="Preformatted TicketList-Log">
-      {res || 'Waiting...'}
+    <div className="Preformatted TicketList-Log"
+         dangerouslySetInnerHTML={html} >
+      {/* {res || 'Waiting...'} */}
     </div>
   )
 }
