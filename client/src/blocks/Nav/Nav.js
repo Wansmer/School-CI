c9f1741f84b902926ca9cef14d46be71570ca063
Wansmer <wansmer@gmail.com>
2020-04-01 16:56:20 +0300

    add easter egg for footer

--- client/src/blocks/Nav/Nav.js
+++ client/src/blocks/Nav/Nav.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 import './Nav.scss';
 
-function Nav(props) {
+const Nav = (props) => {
   return (
     <nav className={props.className + ' Nav'}>
-      <Link to='https://yandex.ru/support/' className="Link Link_color_secondary">Support</Link>
-      <Link to='https://account.shri.yandex' className="Link Link_color_secondary">Learning</Link>
+      <a href='https://yandex.ru/support/' className="Link Link_color_secondary">Support</a>
+      <a href='https://account.shri.yandex' className="Link Link_color_secondary">Learning</a>
     </nav>
   )
 }
