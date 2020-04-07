701cef7f4ba37744eee20cf7e10b7c114ae11f26
Wansmer <wansmer@gmail.com>
2020-04-07 17:06:24 +0300

    delete extra files and clear dependencies

--- server/server.js
+++ server/server.js
@@ -8,16 +8,13 @@ require('dotenv').config();
 app.use(cors());
 app.use(morgan('dev'));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 const routerConf = require('./routes/settings');
 const routerBuild = require('./routes/builds');
 
-app.use(express.static(path.resolve(__dirname, 'static')));
 app.get('/', (req, res) => {
   res.render('index')
 });
+
 app.use('/api/settings', routerConf);
 app.use('/api/builds', routerBuild);
 
