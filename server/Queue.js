85d078d5432cf3b4850379a95fdeebcce496ee18
Wansmer <wansmer@gmail.com>
2020-04-19 15:06:47 +0300

    update README.md

--- server/Queue.js
+++ server/Queue.js
@@ -16,16 +16,9 @@ exports.Queue = class {
 
   update = async () => {
     try {
-      const builds = await api.getBuildList(0, 35);
-      // TODO: прописать рекурсию, если полученная очередь больше 35 шт.
-      //const getAllBuilds = async (offset, limit) => {
-      //  const res = await api.getBuildList(0, 35);
-      //  builds.push(...res);
-      //  if (res.length === limit) {
-      //    await getAllBuilds(limit + 1, limit + limit);
-      //  }
-      //}
-      //await getAllBuilds(0, 35);
+      const offset = 0;
+      const limit = 100;
+      const builds = await api.getBuildList(offset, limit);
       this.add(builds.filter(item => item.status === 'Waiting').reverse());
       return true;
     } catch (error) {
