d5ad7d1c82858751511a39d9c3ad8f100ac2a0fb
Иван Смирнов <ivansmirnov@MacBook-Pro-Ivan.local>
2020-05-03 15:57:32 +0300

    format document

--- client/public/schoolSw.js
+++ client/public/schoolSw.js
@@ -1,16 +1,16 @@
 const CACHE = 'hw-cache-update-refresh';
 
 const cacheData = [
-  '/static/css', 
-  '/static/media', 
-  '/favicon.ico', 
+  '/static/css',
+  '/static/media',
+  '/favicon.ico',
   '/index.html'
 ]
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-      caches.open(CACHE).then((cache) =>
-          cache.addAll(cacheData))
+    caches.open(CACHE).then((cache) =>
+      cache.addAll(cacheData))
   );
 });
 
@@ -18,36 +18,36 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(fromCache(event.request));
   event.waitUntil(
     update(event.request)
-    .then(refresh)
-    .catch((error) => console.error('Failed from fetch: ', error))
+      .then(refresh)
+      .catch((error) => console.error('Failed from fetch: ', error))
   );
 });
 
 function fromCache(request) {
   return caches.open(CACHE).then((cache) =>
-      cache.match(request).then((matching) =>
-          matching || Promise.reject('no-match')
-      ));
+    cache.match(request).then((matching) =>
+      matching || Promise.reject('no-match')
+    ));
 }
 
 function update(request) {
   return caches.open(CACHE).then((cache) =>
-      fetch(request).then((response) =>
-          cache.put(request, response.clone()).then(() => response)
-      )
+    fetch(request).then((response) =>
+      cache.put(request, response.clone()).then(() => response).catch((error) => console.error('Error from update: ', error))
+    )
   );
 }
 
 function refresh(response) {
   console.log('RESPONSE', response);
   return self.clients.matchAll().then((clients) => {
-      clients.forEach((client) => {
-          const message = {
-              type: 'refresh',
-              url: response.url,
-              eTag: response.headers.get('ETag')
-          };
-          client.postMessage(JSON.stringify(message));
-      });
+    clients.forEach((client) => {
+      const message = {
+        type: 'refresh',
+        url: response.url,
+        eTag: response.headers.get('ETag')
+      };
+      client.postMessage(JSON.stringify(message));
+    });
   }).catch((error) => console.error('Error from refresh: ', error));
 }
