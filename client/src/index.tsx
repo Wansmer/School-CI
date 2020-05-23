e9b467013be3c7755340082463eca240059f91f3
Иван Смирнов <ivansmirnov@MacBook-Pro-Ivan.local>
2020-05-23 14:56:24 +0300

    unregister sw

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,15 +9,15 @@ import App from './blocks/App/App';
 
 import './index.scss';
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/schoolSw.js')
-    .then(() => navigator.serviceWorker.ready.then((worker) => {
-      worker.sync.register('syncdata');
-    }))
-    .catch((err) => console.log(err));
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/schoolSw.js')
+//     .then(() => navigator.serviceWorker.ready.then((worker) => {
+//       worker.sync.register('syncdata');
+//     }))
+//     .catch((err) => console.log(err));
+//   });
+// }
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
