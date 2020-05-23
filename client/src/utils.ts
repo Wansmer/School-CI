810695a60416ce6a723af0ee83f55880b22e521d
Иван Смирнов <ivansmirnov@MacBook-Pro-Ivan.local>
2020-05-23 23:18:21 +0300

    fix problex with getHumanDate

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -10,11 +10,16 @@ export const expandClasses = (classes: Classes, blockName: string, elemName: str
   return result.join(' ');
 };
 
-export const getHumanDate = (date: string, locale: string = 'ru') => {
-  // formatDate: any - потому-что нужна проверка на "Invalid Date"
-  const formatDate: any = new Date(date);
-  if (formatDate === 'Invalid Date') return '-- ___., --:--';
-  const settings = { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+export const getHumanDate = (date: string, locale: string = "ru") => {
+  const formatDate: Date = new Date(date);
+
+  if (formatDate.toLocaleString() === "Invalid Date") return "-- ___., --:--";
+  const settings = {
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit"
+  };
   return formatDate.toLocaleString(locale, settings);
 };
 
